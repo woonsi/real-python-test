cbc729aabebbe9bad230f6b57a8dc1d961970ce8
WOON SI <superman.woonsi@gmail.com>
2021-06-05 12:23:19 +0630

    flask-hello-world-v1

--- app.py
+++ app.py
@@ -2,12 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
 @app.route("/")
 @app.route("/hello")
 
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!!???!?!?!?"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael" :
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 if __name__ == "__main__":
     app.run()
     
+
